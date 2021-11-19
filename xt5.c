63bb45e96903f4c7d064bf12f338b2b1c0c445f0
1024AD <1024842535@qq.com>
2021-11-19 15:19:51 +0800

    习题5作业一版本一+

--- xt5.c
+++ xt5.c
@@ -1,11 +1,11 @@
 /* 习题五 第一题*/
 #include<stdio.h>
-#include<sys/types.h>
-#include<unistd.h>
-#include<sys/wait.h>
-#include<sys/stat.h>
-#include<stdlib.h>
-#include<fcntl.h>
+#include<sys/types.h>	/*提供pid_t类型的定义*/
+#include<unistd.h>	/*提供ssize_t类型的定义 read write */ /*提供fork()函数定义*/
+#include<sys/wait.h>	
+#include<sys/stat.h>	/*提供mode_t类型的定义 open()中*/
+#include<stdlib.h>	/*定义exit()h函数*/
+#include<fcntl.h>	/*声明调用open()时的flag常量*/
 int main()
 {	
 	pid_t pid,pid_w;
@@ -36,7 +36,7 @@ int main()
 		sleep(1);				//等待子进程写入完内容
 	//	printf("parent process, PID is%d,n=%d\n",getpid(),n);
 		fd = open( "tmp.txt", O_RDONLY );	//只读方式打开文件
-		if ( fd == -1 )
+		if ( fd == -1 )				//判断是否打开成功
 		{
 			printf("Open file named \"temp.txt\" failed.\n");
 			return -1;
@@ -46,18 +46,18 @@ int main()
 		close( fd );
 		printf( "%s\n", buffer );
 		
-		do{
+		do{					//
 //			pid_w=wait(NULL);
 //			pid_w=waitpid(pid,NULL,0);
-			pid_w=waitpid(pid,NULL,WNOHANG);
-			if(pid_w==0)
+			pid_w=waitpid(pid,NULL,WNOHANG);//加入WNOHANG选项，正常调用并清理掉返回子进程pid，子程序正在运行返回0,调用失败返回-1
+			if(pid_w==0)			//判断子程序是否在运行，如果运行输出“",并等待1s
 			{
 			       	printf("Child process hasn't exited\n");
 				sleep(1);
 			}
 		}
 		while(pid_w==0);
-		printf("Get child %d\n",pid_w);
+		printf("Get child %d\n",pid_w);		//输出清理掉的子程序的pid
 	}
 	return 0;
 }
